0f5ba77bc0800351096ceb1ef7d704e5544c9693
panupong mikada <path301242@gmail.com>
2023-04-11 21:14:39 +0700

    update link

--- app_2/urls.py
+++ app_2/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:id>', views.testid, name='testid'),
     path('home', views.testpage, name='testpage'),
     path('page2/<int:id>', views.testpage2, name='testpage2'),
+    path('page3', views.testpage3, name='testpage3'),
 ]
--- app_2/views.py
+++ app_2/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
+test_data = [
+    {'id': 1, 'txt': 'lorem1'},
+    {'id': 2, 'txt': 'lorem2'}
+]
+
+# test_link = [
+#     'home',
+#     'page2',
+#     'page3'
+# ]
+
 def test(request):
     return HttpResponse('<h1>test=>HttpResponse</h1>')
 
@@ -10,7 +21,19 @@ def testid(request, id):
     return HttpResponse('<h1>test id = ' + str(id) + '</h1>')
 
 def testpage(request):
-    return render(request, 'app_2/index.html')
+    # context = { 'link': test_link}
+    return render(request, 'app_2/index.html')#, context)
 
 def testpage2(request, id):
-    return render(request, 'app_2/page2.html', context={'id':id})
+    # context = { 'id': id, 'link': test_link }
+    one_data = None
+    try:
+        one_data = [ d for d in test_data if d['id'] == id][0]
+    except IndexError:
+        print('No index')
+    context = { 'data': one_data }
+    return render(request, 'app_2/page2.html', context)#, context)
+
+def testpage3(request):
+    context = { 'data': test_data }#, 'link': test_link }
+    return render(request, 'app_2/page3.html', context)
